793bda642df9dfc3e9395fe626295817fa01f00c
Jakub Adamczyk <jkk.adamczyk@gmail.com>
2018-11-26 20:32:01 +0100

    lab5

---
+++ lab5/bst.erl
@@ -0,0 +1,27 @@
+-module(bst).
+-compile(export_all).
+-record(node, {value = nil, left = nil, right = nil}).
+
+insert(nil, Value) ->
+    #node{value=Value, left=nil, right=nil};
+insert(#node{value=Value, left=Left, right=Right}, NewValue) when NewValue < Value ->
+    #node{value=Value, left=insert(Left, NewValue), right=Right};
+insert(#node{value=Value, left=Left, right=Right}, NewValue) when NewValue >= Value ->
+    #node{value=Value, left=Left, right=insert(Right, NewValue)}.
+
+randomBst(0) -> nil;
+randomBst(Length) ->
+    insert(randomBst(Length - 1), rand:uniform(20)).
+
+bstFromList([]) -> nil;
+bstFromList([H|L]) -> 
+    insert(bstFromList(L, nil), H).
+bstFromList([], Tree) ->
+    Tree;
+bstFromList([H|L], Tree) ->
+    insert(bstFromList(L, Tree), H).
+
+inorder(Tree) when Tree#node.value == nil -> [];
+inorder(nil) -> [];
+inorder(Tree) ->
+    inorder(Tree#node.left) ++ [Tree#node.value] ++ inorder(Tree#node.right).
